437ea034ff57e209b233ad650b08977d630138dd
Sylvain Faucherand <syl.faucherand@gmail.com>
2012-09-06 16:15:39 +0200

    adding shell scripts
    start app with mongodb in one command line
    $ sh run.sh

---
+++ run.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo There are $# arguments to $0: $*
+
+hasarg=0
+
+if test $# = 0
+then
+	hasarg=0
+else
+	hasarg=1
+fi
+
+# --- Start node app and optionnally mongodb ---
+
+# Manage options
+
+while test $# -gt 0
+do
+case $1 in
+
+  # Normal option processing
+  
+    -h | --help)
+      # usage and help
+      echo "
+-h, --help	Show options
+-m, --mongod	Launch mongodb server with node app
+      "
+      ;;
+      
+    -m | --mongod)
+      echo "Launch mongod on in new shell (new terminal window)"
+      osascript -e 'tell app "Terminal"
+      	do script "/usr/local/Cellar/mongodb/1.8.2-x86_64/bin/mongod"
+      end tell' & node app
+      ;;
+
+  # Special cases
+    --)
+      break
+      ;;
+    --*)
+      # error unknown (long) option $1
+      ;;
+    -?)
+      # error unknown (short) option $1
+      ;;
+
+  # Done with options
+    *)
+      break
+      ;;
+  esac
+
+  shift
+done
+
+
+# By default, launch node app if no argument passed
+
+if test $hasarg -eq 0
+then
+	node app
+fi
