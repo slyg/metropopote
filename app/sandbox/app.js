597acde713f22946368c41514bd3f3e65f23d621
Sylvain Faucherand <syl.faucherand@gmail.com>
2012-09-07 19:08:49 +0200

    Disactivate tpl engine swig cache in dev mode

--- app/sandbox/app.js
+++ app/sandbox/app.js
@@ -1,7 +1,8 @@
 // Module dependencies
 
 var express = require('express'),
-	cons    = require('consolidate');
+	cons    = require('consolidate'),
+	swig	= require('swig');
     
 // Create http server
 
@@ -10,24 +11,27 @@ var app = module.exports = express();
 // Middleware
 
 app.configure(function(){
- 
-	app.configure(function(){
-		app.set('views', __dirname + '/views');
-		app.engine('html', cons.swig);
-		app.use(express.bodyParser());
-		app.use(express.methodOverride());
-		app.use(app.router);
-		app.use(express.static(__dirname + '/public'));
-	});
-
+	app.engine('html', cons.swig);
+	app.set('views', __dirname + '/views');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
 	app.use(app.router);
+	app.use(express.static(__dirname + '/static'));
+});
+
+app.configure('development', function(){
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	swig.init({cache: false});
+});
+
+app.configure('production', function(){
+	app.use(express.errorHandler());
 });
 
 // Route list
 
 app.get('/', function(req, res) {
-	res.render('home.html', {route : app.route});
+	res.render('home.html', {route : app.route, cache: false});
 });
 
 // Launch http server
