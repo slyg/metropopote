61551f8b2d8462cb7b24623cd41b5f15a5745ad5
Sylvain Faucherand <syl.faucherand@gmail.com>
2012-10-01 17:42:43 +0200

    adding validations to Authenticated_app model

--- app/api/v1/models/Authenticated_app.js
+++ app/api/v1/models/Authenticated_app.js
@@ -5,15 +5,20 @@ module.exports = function(dbconnect){
 
 	var authenticated_appSchema = new Schema({
 		title: { type: String, required: true },
+		url : { type: String, required:true },
 		updated : { type: Date, default: Date.now },
 		created : { type: Date, default: Date.now }
 	});
 
-	var Authenticated_app = dbconnect.model('Authenticated_app', authenticated_app);
+	var Authenticated_app = dbconnect.model('Authenticated_app', authenticated_appSchema);
 	
-	Recipe.schema.path('title').validate(function (value) {
+	Authenticated_app.schema.path('title').validate(function (value) {
 		return /^[a-zA-Z\u00E0-\u00FC -]+$/g.test(value);
 	}, 'Invalid title (letters only)');
+
+	Authenticated_app.schema.path('url').validate(function (value) {
+                return /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/i.test(value);
+        }, 'Invalid title (letters only)');
 
 	return Authenticated_app;
 
