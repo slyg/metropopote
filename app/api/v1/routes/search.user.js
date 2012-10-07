b3182c99b8fa14e086cb0ebd492914610e3eb5f0
Sylvain Faucherand <syl.faucherand@gmail.com>
2012-10-07 22:51:29 +0200

    Adding default pagination (limit & offset) to search

--- app/api/v1/routes/search.user.js
+++ app/api/v1/routes/search.user.js
@@ -7,8 +7,20 @@ module.exports = function(app){
 	// Routes
 		
 	app.get('/' + app.version + '/search/users', function(req, res, next){
-		
-		User.find(req.query, function (err, users) {
+	
+
+		var
+			limit = req.param('limit') || 10,
+			offset = req.param('offset') || 0
+		;
+
+		// removing reserved properties
+
+		var query = req.query;
+		if(query.limit) delete query.limit;
+		if(query.offset) delete query.offset;
+	
+		User.find(query).limit().limit(limit).skip(offset*limit).exec(function (err, users) {
 
 			if (!err) {
 			
